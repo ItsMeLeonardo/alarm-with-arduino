e6df24ebe0e3d0fad9f18eb64baea13c1797cbda
Carlos Leonardao <74982565+ItsMeLeonardo@users.noreply.github.com>
2021-11-12 13:41:50 -0500

    [ADD] 🪐 file proteus and format the code

--- code/bin/preproc/ctags_target_for_gcc_minus_e.cpp
+++ code/bin/preproc/ctags_target_for_gcc_minus_e.cpp
@@ -1,68 +1,78 @@
 # 1 "e:\\University\\Ciclo IV\\electronic\\project\\code-circuit\\code\\alarm.ino"
 # 2 "e:\\University\\Ciclo IV\\electronic\\project\\code-circuit\\code\\alarm.ino" 2
 LiquidCrystal lcd(11, 10, 9, 8, 7, 6);
-int val;
-int tempPin = 1;
+
+const int timeDelay = 1000;
+const int refrigerationMotor = 12;
+const int ledOfNormalTemperature = 13;
 
 void setup()
 {
+  // define 16col and 2row
   lcd.begin(16, 2);
-  Serial.begin(9600);
+
+  // define the bit transfer speed per second
+  Serial.begin(9600); // 9600 baud/s
+
+  // input of temperature sensor
   pinMode(A1, 0x0);
 
-  // Ping of LED
-  pinMode(13, 0x1);
-
   // Ping of refrigeration
-  pinMode(12, 0x1);
+  pinMode(refrigerationMotor, 0x1);
+
+  // Ping of LED
+  pinMode(ledOfNormalTemperature, 0x1);
 }
 
 void loop()
 {
-  val = analogRead(A1);
+  const int valueOfSensor = analogRead(A1);
   lcd.setCursor(0, 0);
 
   // transform the input value into a C°
-  float mv = (val / 1024.0) * 5000.0;
-  float temp = mv / 10;
+  float mv = (valueOfSensor / 1024.0) * 5000.0;
+  float temperature = mv / 10;
 
   // this print in virtual terminal
   Serial.print("Temperature = ");
-  Serial.print(temp);
-  lcd.print("Temp C°: ");
-  lcd.print(temp);
-  // lcd.noBlink();
+  Serial.print(temperature);
+
+  // print in LCD
+  lcd.print("Temperature: ");
+  lcd.print(temperature);
 
   // NORMAL TEMPERATURE range is 0-40
-  if (temp < 40)
+  if (temperature < 40)
   {
-    digitalWrite(13, 0x1);
-    digitalWrite(12, 0x0);
+    digitalWrite(ledOfNormalTemperature, 0x1);
+    digitalWrite(refrigerationMotor, 0x0);
     lcd.setCursor(0, 5);
     lcd.print("Stable 😃");
-    delay(1000);
+    delay(timeDelay);
     lcd.clear();
+    /*do nothing() 📲*/
   }
 
   // ALERT increase of temperature
-  if (temp > 40 && temp < 60)
+  if (temperature >= 40 && temperature <= 60)
   {
-    digitalWrite(12, 0x0);
-    digitalWrite(13, 0x1);
+    digitalWrite(ledOfNormalTemperature, 0x0);
+    digitalWrite(refrigerationMotor, 0x1);
     lcd.setCursor(0, 5);
-    lcd.print("Alert");
-    delay(1000);
+    lcd.print("Alert!! 🥵");
+    delay(timeDelay);
     lcd.clear();
+    /*send alert() 📲*/
   }
 
   // FIRE ALERT
-  if (temp > 60)
+  if (temperature > 60)
   {
-    digitalWrite(13, 0x0);
-    digitalWrite(12, 0x0);
+    digitalWrite(ledOfNormalTemperature, 0x0);
+    digitalWrite(refrigerationMotor, 0x0);
     lcd.setCursor(0, 5);
     lcd.print("FIRE ALERT !!!");
-    delay(1000);
+    delay(timeDelay);
     lcd.clear();
     /*callFireMan() 📲*/
   }
--- code/bin/sketch/alarm.ino.cpp
+++ code/bin/sketch/alarm.ino.cpp
@@ -2,73 +2,83 @@
 #line 1 "e:\\University\\Ciclo IV\\electronic\\project\\code-circuit\\code\\alarm.ino"
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(11, 10, 9, 8, 7, 6);
-int val;
-int tempPin = 1;
 
-#line 6 "e:\\University\\Ciclo IV\\electronic\\project\\code-circuit\\code\\alarm.ino"
+const int timeDelay = 1000;
+const int refrigerationMotor = 12;
+const int ledOfNormalTemperature = 13;
+
+#line 8 "e:\\University\\Ciclo IV\\electronic\\project\\code-circuit\\code\\alarm.ino"
 void setup();
-#line 19 "e:\\University\\Ciclo IV\\electronic\\project\\code-circuit\\code\\alarm.ino"
+#line 26 "e:\\University\\Ciclo IV\\electronic\\project\\code-circuit\\code\\alarm.ino"
 void loop();
-#line 6 "e:\\University\\Ciclo IV\\electronic\\project\\code-circuit\\code\\alarm.ino"
+#line 8 "e:\\University\\Ciclo IV\\electronic\\project\\code-circuit\\code\\alarm.ino"
 void setup()
 {
+  // define 16col and 2row
   lcd.begin(16, 2);
-  Serial.begin(9600);
+
+  // define the bit transfer speed per second
+  Serial.begin(9600); // 9600 baud/s
+
+  // input of temperature sensor
   pinMode(A1, INPUT);
 
-  // Ping of LED
-  pinMode(13, OUTPUT);
-
   // Ping of refrigeration
-  pinMode(12, OUTPUT);
+  pinMode(refrigerationMotor, OUTPUT);
+
+  // Ping of LED
+  pinMode(ledOfNormalTemperature, OUTPUT);
 }
 
 void loop()
 {
-  val = analogRead(A1);
+  const int valueOfSensor = analogRead(A1);
   lcd.setCursor(0, 0);
 
   // transform the input value into a C°
-  float mv = (val / 1024.0) * 5000.0;
-  float temp = mv / 10;
+  float mv = (valueOfSensor / 1024.0) * 5000.0;
+  float temperature = mv / 10;
 
   // this print in virtual terminal
   Serial.print("Temperature = ");
-  Serial.print(temp);
-  lcd.print("Temp C°: ");
-  lcd.print(temp);
-  // lcd.noBlink();
+  Serial.print(temperature);
+
+  // print in LCD
+  lcd.print("Temperature: ");
+  lcd.print(temperature);
 
   // NORMAL TEMPERATURE range is 0-40
-  if (temp < 40)
+  if (temperature < 40)
   {
-    digitalWrite(13, HIGH);
-    digitalWrite(12, LOW);
+    digitalWrite(ledOfNormalTemperature, HIGH);
+    digitalWrite(refrigerationMotor, LOW);
     lcd.setCursor(0, 5);
     lcd.print("Stable 😃");
-    delay(1000);
+    delay(timeDelay);
     lcd.clear();
+    /*do nothing() 📲*/
   }
 
   // ALERT increase of temperature
-  if (temp > 40 && temp < 60)
+  if (temperature >= 40 && temperature <= 60)
   {
-    digitalWrite(12, LOW);
-    digitalWrite(13, HIGH);
+    digitalWrite(ledOfNormalTemperature, LOW);
+    digitalWrite(refrigerationMotor, HIGH);
     lcd.setCursor(0, 5);
-    lcd.print("Alert");
-    delay(1000);
+    lcd.print("Alert!! 🥵");
+    delay(timeDelay);
     lcd.clear();
+    /*send alert() 📲*/
   }
 
   // FIRE ALERT
-  if (temp > 60)
+  if (temperature > 60)
   {
-    digitalWrite(13, LOW);
-    digitalWrite(12, LOW);
+    digitalWrite(ledOfNormalTemperature, LOW);
+    digitalWrite(refrigerationMotor, LOW);
     lcd.setCursor(0, 5);
     lcd.print("FIRE ALERT !!!");
-    delay(1000);
+    delay(timeDelay);
     lcd.clear();
     /*callFireMan() 📲*/
   }
